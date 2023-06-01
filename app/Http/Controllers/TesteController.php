0a74f8a93a80aa6ac1e4f9e50040dca090cba48c
Roger Santos Ferreira <rogersantosferreira@gmail.com>
2023-06-01 09:18:57 -0300

    commit inicial

---
+++ app/Http/Controllers/TesteController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Aluno;
+
+class TesteController extends Controller
+{
+    public function teste() {
+        return 'teste';
+    }
+}
