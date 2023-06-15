9eb26b6735cda6c2fe51b19d426f1ac89fc333b5
Roger Santos Ferreira <rogersantosferreira@gmail.com>
2023-06-15 20:48:31 -0300

    primeiro crud completo

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TesteController;
+use App\Http\Controllers\AlunoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,9 +15,15 @@ use App\Http\Controllers\TesteController;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
+Route::get('/', function() {
     return view('welcome');
 });
+
+Route::get('/alunos',               [AlunoController::class, 'index'])->name('alunos.index');
+Route::get('/alunos/create',        [AlunoController::class, 'create'])->name('alunos.create');
+Route::post('/alunos/create',       [AlunoController::class, 'store'])->name('alunos.store');
+Route::get('/alunos/edit/{id}',     [AlunoController::class, 'edit'])->name('alunos.edit');
+Route::post('/alunos/update/{id}',  [AlunoController::class, 'update'])->name('alunos.update');
+Route::get('/alunos/delete/{id}',   [AlunoController::class, 'delete'])->name('alunos.delete');
 
 Route::get('/teste', [TesteController::class, 'teste'])->name('teste');
